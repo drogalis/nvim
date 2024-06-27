17903cc0ac726880939320dbe04bd267b6bfbce2
Andrew Drogalis <108765079+andrew-drogalis@users.noreply.github.com>
2024-06-27 09:31:17 -0400

    Fixed error in markdown-preview

--- lua/drogalis/plugins/markdown-preview.lua
+++ lua/drogalis/plugins/markdown-preview.lua
@@ -1,8 +1,9 @@
 return {
 	"iamcco/markdown-preview.nvim",
 	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+	ft = { "markdown" },
 	build = function()
 		vim.fn["mkdp#util#install"]()
+		-- Fix Error with ":call mkdp#until#install()"
 	end,
-	ft = { "markdown" },
 }
