a70d184bcfdb0d323eb29670a757156b5856900c
Andrew Drogalis <108765079+andrew-drogalis@users.noreply.github.com>
2024-06-30 08:44:25 -0400

    Activated Bufferline

--- lua/drogalis/plugins/bufferline.lua
+++ lua/drogalis/plugins/bufferline.lua
@@ -2,4 +2,20 @@ return {
 	"akinsho/bufferline.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	version = "*",
+	config = function()
+		require("bufferline").setup({
+			options = {
+				mode = "buffers",
+				offsets = {
+					{
+						filetype = "NvimTree",
+						text = "File Explorer",
+						highlight = "Directory",
+						separator = true,
+					},
+				},
+			},
+		})
+		vim.keymap.set("n", "<leader>bl", ":BufferLinePick<CR>")
+	end,
 }
