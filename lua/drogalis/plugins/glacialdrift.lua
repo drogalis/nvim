09a12112541fc7932f8b2483149b4a9ee021bfa6
Andrew Drogalis <108765079+drogalis@users.noreply.github.com>
2025-04-09 22:52:39 +0000

    prelim support for glacialdrift

---
+++ lua/drogalis/plugins/glacialdrift.lua
@@ -0,0 +1,9 @@
+return {
+    "drogalis/glacialdrift.nvim",
+    lazy = false,
+	priority = 1000,
+	config = function()
+		--vim.cmd("syntax enable")
+		--vim.cmd.colorscheme("glacialdrift")
+	end,
+}
